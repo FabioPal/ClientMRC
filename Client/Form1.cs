b96f949ada249db1cd5295c1a772ac0997fbd6c4
Fabio <fabiopalumbo@msn.com>
2016-05-11 15:21:39 +0200

    Primo commit

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -56,11 +56,6 @@ namespace Client
                         // Send the data through the socket.
                         int bytesSent = sender.Send(msg);
 
-                        // Receive the response from the remote device.
-                        //int bytesRec = sender.Receive(bytes);
-                        //Console.WriteLine("Echoed test = {0}",
-                        //     Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
                         // Release the socket.
                         sender.Shutdown(SocketShutdown.Both);
                         sender.Close();
