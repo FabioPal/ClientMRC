691fa6336b07d7d3984fc2773084ef473af90234
Franco <Franco@FRANCO-PC>
2016-05-11 15:19:26 +0200

    Aggiungere i file di progetto.

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipBox = new System.Windows.Forms.TextBox();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.connectionBtn = new System.Windows.Forms.Button();
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.msgBox = new System.Windows.Forms.TextBox();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ipBox
+            // 
+            this.ipBox.Location = new System.Drawing.Point(137, 36);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.Size = new System.Drawing.Size(100, 20);
+            this.ipBox.TabIndex = 0;
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(137, 86);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(100, 20);
+            this.portBox.TabIndex = 1;
+            // 
+            // connectionBtn
+            // 
+            this.connectionBtn.Location = new System.Drawing.Point(212, 290);
+            this.connectionBtn.Name = "connectionBtn";
+            this.connectionBtn.Size = new System.Drawing.Size(116, 29);
+            this.connectionBtn.TabIndex = 2;
+            this.connectionBtn.Text = "Connettiti";
+            this.connectionBtn.UseVisualStyleBackColor = true;
+            this.connectionBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ipLabel
+            // 
+            this.ipLabel.AutoSize = true;
+            this.ipLabel.Location = new System.Drawing.Point(70, 43);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(61, 13);
+            this.ipLabel.TabIndex = 3;
+            this.ipLabel.Text = "Indirizzo IP:";
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(43, 93);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(88, 13);
+            this.portLabel.TabIndex = 4;
+            this.portLabel.Text = "Numero di porta: ";
+            // 
+            // msgBox
+            // 
+            this.msgBox.Location = new System.Drawing.Point(42, 158);
+            this.msgBox.Multiline = true;
+            this.msgBox.Name = "msgBox";
+            this.msgBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.msgBox.Size = new System.Drawing.Size(237, 115);
+            this.msgBox.TabIndex = 5;
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Location = new System.Drawing.Point(43, 142);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(58, 13);
+            this.msgLabel.TabIndex = 6;
+            this.msgLabel.Text = "Messaggio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 345);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.msgBox);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.ipLabel);
+            this.Controls.Add(this.connectionBtn);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.ipBox);
+            this.Name = "Form1";
+            this.Text = "Sensor - Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipBox;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Button connectionBtn;
+        private System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.TextBox msgBox;
+        private System.Windows.Forms.Label msgLabel;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object senderObj, EventArgs e)
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.
+            try
+            {
+                IPAddress ipAddress;
+                //Controlla se l'indirizzo IP è in formato valido
+                if (!IPAddress.TryParse((ipBox.Text), out ipAddress))
+                {
+                    MessageBox.Show("Inserisci un indirizzo IP valido", "Errore");
+                }
+                else
+                {
+                    // Establish the remote endpoint for the socket.
+                    UInt16 portNum = UInt16.Parse(portBox.Text);
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, portNum);
+                    // Create a TCP/IP  socket.
+                    Socket sender = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Stream, ProtocolType.Tcp);
+
+                    // Connect the socket to the remote endpoint. Catch any errors.
+                    try
+                    {
+                        sender.Connect(remoteEP);
+
+                        Console.WriteLine("Socket connected to {0}",
+                            sender.RemoteEndPoint.ToString());
+
+                        // Encode the data string into a byte array.
+                        byte[] msg = Encoding.ASCII.GetBytes(msgBox.Text + "<EOF>");
+
+                        // Send the data through the socket.
+                        int bytesSent = sender.Send(msg);
+
+                        // Receive the response from the remote device.
+                        //int bytesRec = sender.Receive(bytes);
+                        //Console.WriteLine("Echoed test = {0}",
+                        //     Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                        // Release the socket.
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                    }
+
+                    catch (ArgumentNullException ane)
+                    {
+                        MessageBox.Show("Errore nell'invio del messaggio", "Errore");
+                        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    }
+                    catch (SocketException se)
+                    {
+                        MessageBox.Show("Errore nella socket", "Errore");
+                        Console.WriteLine("SocketException : {0}", se.ToString());
+                    }
+                    catch (Exception exce)
+                    {
+                        MessageBox.Show("Errore non previsto", "Errore");
+                        Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    }
+                }
+
+            }
+            catch (Exception exce)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
